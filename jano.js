640440728190c1b087cf9919f185fb4c7df0a628
Developer-Hama <93672668+Developer-Hama@users.noreply.github.com>
2021-11-19 19:32:33 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const jano = new Discord.Client();
 //////// playing or dnd  streaming ////////
-jano.on("ready", () => {
+jano.on("ready", (KP UP) => {
   console.log(`Online In Servers`);
-  let statuses = [`JUST ALLAH`];
+  let statuses = [`KP UP`];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     jano.user.setActivity(STREAMING, {
@@ -13,9 +13,9 @@ jano.on("ready", () => {
   }, 2000);
 });
 ///////////////////////
-jano.on("ready", () => {
+jano.on("ready", (KP UP) => {
   var join = jano.channels.get(""); /// id voice
   if (join) join.join();
 });
 //////////////// tokn lera da bne
-jano.login("");
+jano.login("ODM1NDY4MTE0Njk0MjQyMzA0.YZfRlQ.czE2JHQ6pYgPKHRtKLI1KvumI8U");
